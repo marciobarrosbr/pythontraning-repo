9cab4374b5eff2769af96d2f25e0503771c1f495
Marcio Fernando Batista Barros <marcio.barros@ibhservicos.com.br>
2023-01-30 13:41:39 -0300

    Atualização diária
    exercise 6, 7 an 8

--- Algoritimos_lista_exercicios.py
+++ Algoritimos_lista_exercicios.py
@@ -16,3 +16,40 @@
 #answer = a + b
 #print("A soma entre " + str(a) + " e " + str(b) + " é igual a " + str(answer))
 
+#EXERCICIO 5)
+#a = float(input("input the fist number: "))
+#b = float(input("Input the second number: "))
+#answer = (a + b)  / 2
+#print("The average between " + str(a) +" e " + str(b) + " it´s the same as " + str(answer))
+
+#EXERCICIO 6)
+#x = int(input("input the number: "))
+#before = x - 1
+#after = x + 1
+#print("The predecessor of " + str(x) +" is " + str(before))
+#print("The sucessor of " + str(x) + " is " + str(after))
+ 
+#EXERCISE 7)
+#x = float(input("input the number: "))
+#y = x * 2
+#z = x / 3
+#print("the double of " + str(x) + " is " + str(y))
+#print("the third par of " + str(x) + " is " + str(z))
+
+#exercise 8)
+#x = float(input("input the distance in meters: "))
+#print("the distance of "+ str(x) + " m " + "stands for")
+#a = x *0.001
+#b = x * 0.01
+#c = x * 0.1
+#d = x * 10
+#e = x * 100
+#f = x * 1000
+#print(str(a) + " km ")
+#print(str(b) + " Hm")
+#print(str(c) + " Dam")
+#print(str(d) + " dm")
+#print(str(e) + " cm")
+#print(str(f) + " mm")
+
+#EXERCISE 9)
